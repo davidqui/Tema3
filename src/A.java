c427db3af24a273470993d6be5d4430826ee617a
davidqui <davidquijanoramos@gmail.com>
2018-02-11 11:16:27 -0500

    iniciamos

---
+++ src/A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class A extends A1{
+   // A B;
+    public A(){
+        
+        System.out.println("impresion de A");
+    }
+    
+}
---
+++ src/HerenciaApp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author david
+ */
+public class HerenciaApp {
+    public static void main(String[] args) {
+        A1 a = new A();
+        B b = new B();          /// IMPRIME EL METODO DE LA CLASE "B" Y DE LA SUPERCLASE "A"
+        A b1 = new B();
+        
+        ///     %%%%%%%%%%%%%%%%%%%%%%%%%%%555
+        
+        System.out.println();
+    }
+    
+}
